fde7f1aa0d746fda4b08e20759aa57913f7955ee
Vidhi Sharma <vidhisharma921@gmail.com>
2021-07-03 00:11:05 +0530

    Python

---
+++ ch8_2.py
@@ -0,0 +1,5 @@
+loona= 'A lot                   of spaces'
+print(loona.split())
+line= 'first;second;third'
+print(line.split())
+print(line.split(";"))
---
+++ ch8_3_double_split_patterns.py
@@ -0,0 +1,10 @@
+mail= "someone someday@1.2.3.ut mailed box on saturday"
+words= mail.split()
+print(words)
+email= words[1]
+print(email)
+# now,
+
+a= email.split('@')
+print(a)
+print(a[1])
